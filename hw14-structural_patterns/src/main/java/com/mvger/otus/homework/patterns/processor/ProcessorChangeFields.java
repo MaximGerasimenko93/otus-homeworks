434e6d60b8d35686cf6e615b8618f3ef706600e8
MaximGerasimenko93 <gerasimx93@gmail.com>
2023-12-26 16:33:30 +0700

    Add the realization for Process and made a test

---
+++ hw14-structural_patterns/src/main/java/com/mvger/otus/homework/patterns/processor/ProcessorChangeFields.java
@@ -0,0 +1,15 @@
+package com.mvger.otus.homework.patterns.processor;
+
+import com.mvger.otus.homework.patterns.model.Message;
+
+public class ProcessorChangeFields implements Processor {
+
+    @Override
+    public Message process(Message message) {
+        Message.Builder builder = message.toBuilder();
+        String field11 = builder.getField11();
+        builder.field11(builder.getField12());
+        builder.field12(field11);
+        return builder.build();
+    }
+}
