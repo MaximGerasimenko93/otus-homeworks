a60b9c21c6d986abd7f9d81efdd403c61a011e5e
MaximGerasimenko93 <gerasimx93@gmail.com>
2024-01-09 17:01:53 +0700

    Add values for field11, field12 and field13

--- hw14-structural_patterns/src/main/java/com/mvger/otus/homework/patterns/HomeWork.java
+++ hw14-structural_patterns/src/main/java/com/mvger/otus/homework/patterns/HomeWork.java
@@ -3,10 +3,13 @@ package com.mvger.otus.homework.patterns;
 import com.mvger.otus.homework.patterns.handler.ComplexProcessor;
 import com.mvger.otus.homework.patterns.listener.ListenerPrinterConsole;
 import com.mvger.otus.homework.patterns.model.Message;
+import com.mvger.otus.homework.patterns.model.ObjectForMessage;
 import com.mvger.otus.homework.patterns.processor.LoggerProcessor;
+import com.mvger.otus.homework.patterns.processor.ProcessorChangesFields;
 import com.mvger.otus.homework.patterns.processor.ProcessorConcatsFields;
 import com.mvger.otus.homework.patterns.processor.ProcessorUppersField10;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomeWork {
@@ -14,7 +17,8 @@ public class HomeWork {
     public static void main(String[] args) {
         var processors = List.of(
                 new ProcessorConcatsFields(),
-                new LoggerProcessor(new ProcessorUppersField10())
+                new LoggerProcessor(new ProcessorUppersField10()),
+                new ProcessorChangesFields()
         );
 
         var complexProcessor = new ComplexProcessor(processors, ex -> {
@@ -29,6 +33,9 @@ public class HomeWork {
                 .field3("field3")
                 .field6("field6")
                 .field10("field10")
+                .field11("field11")
+                .field12("field12")
+                .field13(new ObjectForMessage(new ArrayList<>()))
                 .build();
 
         var result = complexProcessor.handle(message);
